21ab964e395c310c6db338c1306db76045a2bf20
Your Name <your.email@example.com>
2025-05-17 19:17:08 +0200

    Revert to Vercel-compatible configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/MyPortfolio',
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
